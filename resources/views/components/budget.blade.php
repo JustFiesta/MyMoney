0e5de74ecc5cb4f696c52ef6cd6091e32ebd7116
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-17 19:41:48 +0200

    Skeches os subsites and views

---
+++ resources/views/components/budget.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+            {{ __("Szybki dostęp") }}
+        </div>
+    </div>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+            {{ __("Wydatki w *miesiąc*") }}
+        </div>
+    </div>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+            {{ __("Cele") }}
+        </div>
+    </div>
+</div>
