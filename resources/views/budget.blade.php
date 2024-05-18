4909c2f811386902f64feb69f5016b142c9984a1
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-18 14:03:03 +0200

    Translation continuation

--- resources/views/budget.blade.php
+++ resources/views/budget.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Your budget for *month*') }}
+            {{ __('Twój budżet w miesiącu *miesiąc*') }}
         </h2>
     </x-slot>
 
