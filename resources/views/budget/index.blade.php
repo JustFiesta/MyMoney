525019577229104bce8469bac154c8e920ebc7ad
JustFiesta <99048876+JustFiesta@users.noreply.github.com>
2024-05-20 12:48:39 +0200

    Viewing data from db - categories, expences, goals

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -4,7 +4,8 @@
             {{ __("Szybki dostęp") }}
         </div>
         <div class="flex flex-col text-gray-600">
-            @if (count($categories) > 0)
+            {{-- @dd($categories) --}}
+            @if (@isset($categories) && !$categories->isEmpty())
                 @foreach ($categories as $category)
                     <button>{{ $category }}</button>
                 @endforeach
@@ -17,10 +18,30 @@
         <div class="p-6 text-gray-600 dark:text-gray-100">
             {{ __("Wydatki w *miesiąc*") }}
         </div>
+        <div class="flex flex-col text-gray-600">
+            @if (@isset($expences) && !$expences->isEmpty())
+                @foreach ($expences as $expence)
+                    <p>{{ $expence->amount; }}</p>
+                    <p>{{ $expence->created_at; }}</p>
+                @endforeach
+            @else
+                <p>Brak wydatków dla twojego konta.</p>
+            @endif
+        </div>
     </div>
     <div class="bg-white dark:bg-gray-800 overflow-hidden w-full lg:w-5/12">
         <div class="p-6 text-gray-600 dark:text-gray-100">
             {{ __("Cele") }}
+        </div>
+        <div class="flex flex-col text-gray-600">
+            @if (@isset($goals) && !$goals->isEmpty())
+                @foreach ($goals as $goal)
+                    <p>{{ $goal->amount; }}</p>
+                    <p>{{ $goal->content; }}</p>
+                @endforeach
+            @else
+                <p>Brak wydatków dla twojego konta.</p>
+            @endif
         </div>
     </div>
 </div>
