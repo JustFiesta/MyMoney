f189a0f014660f3bb820da5cabaff69ee7beef61
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-23 00:12:25 +0200

    Added div for billing period

--- resources/views/view-budget.blade.php
+++ resources/views/view-budget.blade.php
@@ -5,9 +5,20 @@
         </h2>
     </x-slot>
 
+    <div class="py-2 md:py-4 lg:py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex items-center justify-between p-3 md:p-6 lg:p-6 text-black-900 dark:text-gray-300">
+                    <p>Okres rozliczeniowy: {{ now()->translatedFormat('F') }}</p>
+                    ikona kalendarza
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div>
         @if (session('status'))
-            <div class="max-w-7xl mx-auto pt-2 lg:pt-4">
+            <div class="max-w-7xl mx-auto">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="bg-green-500 text-white p-4 rounded-lg">
                             {{ session('status') }}
@@ -15,7 +26,8 @@
                 </div>
             </div>
         @endif
-
-        @include('budget.view')
     </div>
+
+    @include('budget.view')
+
 </x-app-layout>
