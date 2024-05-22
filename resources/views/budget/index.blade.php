989e76b21f9dd894a52ff7874dcee34a709c8022
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-22 20:17:14 +0200

    More styling to outcome table, added summary

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -1,73 +1,79 @@
 <div class="flex flex-col lg:flex-row justify-between w-full text-center">
-    <div class="bg-white dark:bg-gray-800 overflow-hidden w-full lg:w-5/12 pb-2 ">
+    <div class="flex flex-col bg-white dark:bg-gray-800 overflow-hidden w-full lg:w-5/12 pb-2">
         <div class="p-3 lg:p-6 text-gray-600 dark:text-gray-100">
-            <h2 class="font-bold text-l">Szybki dostęp</h2>
+            <h2 class="font-bold text-xl">Szybki dostęp</h2>
+            <button class="py-4 px-4 mt-2 w-max text-lg rounded-full loginButton font-bold" onclick="filterExpenses('all')">Pokaż wszystko</button>
         </div>
-        <div class="flex flex-col text-gray-400">
-            @if (@isset($categories) && !$categories->isEmpty())
+        <div class="flex flex-wrap text-gray-400">
+            @if (isset($categories) && !$categories->isEmpty())
                 @foreach ($categories as $category)
-                    <button>{{ $category }}</button>
+                    <div class="w-1/2 p-2">
+                        <button class="py-4 px-2 text-lg w-full rounded-full bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onclick="filterExpenses('{{ $category }}')">{{ $category }}</button>
+                    </div>
                 @endforeach
             @else
                 <p>Brak kategorii dla twojego konta.</p>
             @endif
         </div>
     </div>
-    <div class="bg-white dark:bg-gray-800 overflow-hidden w-full pb-2 border-t-2 border-b-2 lg:border-t-0 lg:border-b-0 lg:border-l-2 lg:border-r-2 border-grey-600">
+    <div class="flex flex-col items-center bg-white dark:bg-gray-800 overflow-hidden w-full pb-2 border-t-2 border-b-2 lg:border-t-0 lg:border-b-0 lg:border-l-2 lg:border-r-2 border-grey-600">
         <div class="p-3 lg:p-6 text-gray-600 dark:text-gray-100">
-            <h2 class="font-bold text-l">Wydatki w *miesiąc*</h2>
+            <h2 class="font-bold text-xl">Wydatki w *miesiąc*</h2>
         </div>
-        <div class="flex flex-col text-gray-400">
-            <div class="text-gray-400">
-                <div class="flex flex-col items-center text-gray-400">
-                    @if (isset($expenses) && !$expenses->isEmpty())
-                        <table class="min-w-full bg-white dark:bg-gray-800">
-                            <thead>
-                                <tr>
-                                    <th class="py-2 font-extrabold">Kwota</th>
-                                    <th class="py-2 font-extrabold">Data</th>
-                                    <th class="py-2 font-extrabold">Akcje</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($expenses as $expense)
-                                    <tr>
-                                        <td class="py-2 font-semibold">{{ $expense->amount }}</td>
-                                        <td class="py-2">{{ $expense->created_at }}</td>
-                                        <td class="py-2">
-                                            <form action="{{ route('finances.destroy', $expense->id) }}" method="POST" style="display: inline;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" onclick="return confirm('Are you sure?')" class="text-red-600 font-semibold">Usuń</button>
-                                            </form>
-                                            <a href="{{ route('finances.edit', $expense->id) }}" class="text-blue-600 ml-2 font-semibold">Edytuj</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    @else
-                        <p>Brak wydatków dla twojego konta.</p>
-                    @endif
-                    <div class="mt-4 p-4 bg-gray-200 dark:bg-gray-700 rounded-md w-2/3">
-                        <p class="font-semibold text-gray-800 dark:text-gray-200">Podsumowanie:</p>
-                        <p class="text-gray-600 dark:text-gray-400">Suma wydatków: {{ $totalExpenses }} PLN</p>
-                        <p class="text-gray-600 dark:text-gray-400">Suma przychodów: {{ $totalIncomes }} PLN</p>
-                        <p class="text-gray-600 dark:text-gray-400">Różnica (przychody - wydatki): {{ $balance }} PLN</p>
-                    </div>
-                </div>
+        <div class="flex flex-col w-full text-gray-400">
+            @if (isset($expences) && !$expences->isEmpty())
+                <table class="bg-white dark:bg-gray-800">
+                    <thead>
+                        <tr>
+                            <th class="py-2 w-1/3 text-lg font-extrabold">Kwota</th>
+                            <th class="py-2 w-1/3 text-lg font-extrabold">Data</th>
+                            <th class="py-2 w-1/3 text-lg font-extrabold">Akcje</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($expences as $expence)
+                            <tr class="expense-row" data-category="{{ $expence->category }}">
+                                <td class="py-2 text-lg font-semibold">{{ $expence->amount }}</td>
+                                <td class="py-2 text-lg">{{ $expence->created_at->format('d.m.Y') }}</td>
+                                <td class="py-2 text-lg ">
+                                    <form class="inline">
+                                        <button type="submit" onclick="return confirm('Are you sure?')" class="text-red-600 font-bold">Usuń</button>
+                                    </form>
+                                    <a href="#" class="text-blue-600 ml-2 font-bold">Edytuj</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <p>Brak wydatków dla twojego konta.</p>
+            @endif
+        </div>
+        <div class="summary mt-4 p-4 w-9/12 bg-gray-200 dark:bg-gray-700 rounded-md">
+            <p class="font-semibold text-xl text-gray-800 dark:text-gray-200">Podsumowanie:</p>
+            <div class="flex justify-between py-1">
+                <p class="text-xl text-gray-600 dark:text-gray-400">Wydatki</p>
+                <p class="text-xl text-red-600">{{ $totalExpenses }} PLN</p>
+            </div>
+            <div class="income-summary flex justify-between py-1">
+                <p class="text-xl text-gray-600 dark:text-gray-400">Przychody</p>
+                <p class="text-xl text-green-600">{{ $totalIncomes }} PLN</p>
+            </div>
+            <div class="balance-summary flex justify-between py-1">
+                <p class="text-xl text-gray-600 dark:text-gray-400">Różnica</p>
+                <p class="text-2xl font-bold text-{{ $balance >= 0 ? 'green' : 'red' }}-600">{{ $balance }} PLN</p>
             </div>
         </div>
     </div>
     <div class="bg-white dark:bg-gray-800 overflow-hidden w-full lg:w-5/12 pb-2">
         <div class="p-3 lg:p-6 text-gray-600 dark:text-gray-100">
-            <h2 class="font-bold text-l">Cele</h2>
+            <h2 class="font-bold text-xl">Cele</h2>
         </div>
         <div class="flex flex-col text-gray-400">
             @if (@isset($goals) && !$goals->isEmpty())
                 @foreach ($goals as $goal)
-                    <p>{{ $goal->amount; }}</p>
-                    <p>{{ $goal->content; }}</p>
+                    <p>{{ $goal->amount }}</p>
+                    <p>{{ $goal->content }}</p>
                 @endforeach
                 <button>Edytuj cele</button>
             @else
