f50d8dcb256e0aad6c4cb58c5b445382e508e2cf
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-22 21:01:16 +0200

    Added main font in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,12 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased" 
+    <body class="welcomeFont antialiased" 
         x-data="{ darkMode: localStorage.getItem('dark') === 'true' }"
         x-init="$watch('darkMode', val => localStorage.setItem('dark', val))"
         x-bind:class="{'dark': darkMode}"
