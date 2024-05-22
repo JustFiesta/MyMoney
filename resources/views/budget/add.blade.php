f4d63a79c726e5a12ef6253b5d70e1de87334d62
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-22 23:50:26 +0200

    Cancel buttons for add/edit forms (budget and goal)

--- resources/views/budget/add.blade.php
+++ resources/views/budget/add.blade.php
@@ -20,7 +20,10 @@
                         <input type="text" id="category" name="category" class="mt-1 block w-full pl-3 pr-3 py-2 border border-gray-300 dark:border-gray-600 bg-inherit rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
                     </div>
                     <div class="flex items-center justify-end">
-                        <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button onclick="window.history.back()" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                            Anuluj
+                        </button>
+                        <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white uppercase bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-150">
                             Dodaj
                         </button>
                     </div>
--- resources/views/goals/edit.blade.php
+++ resources/views/goals/edit.blade.php
@@ -7,14 +7,17 @@
                     @method('PUT')
                     <div class="mb-4">
                         <label for="amount" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Kwota</label>
-                        <input type="number" id="amount" name="amount" class="mt-1 block w-full pl-3 pr-3 py-2 bg-inherit border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" value="{{ $goal->amount }}" required>
+                        <input type="number" id="amount" name="amount" class="mt-1 block w-full pl-3 pr-3 py-2 border border-gray-300 dark:border-gray-600 bg-inherit rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" value="{{ $goal->amount }}" required>
                     </div>
                     <div class="mb-4">
                         <label for="content" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Cel</label>
-                        <input type="text" id="content" name="content" class="mt-1 block w-full pl-3 pr-3 py-2 bg-inherit border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" value="{{ $goal->content }}" required>
+                        <input type="text" id="content" name="content" class="mt-1 block w-full pl-3 pr-3 py-2 border border-gray-300 dark:border-gray-600 bg-inherit rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" value="{{ $goal->content }}" required>
                     </div>
                     <div class="flex items-center justify-end">
-                        <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button onclick="window.history.back()" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                            Anuluj
+                        </button>
+                        <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white uppercase bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-150">
                             Zapisz
                         </button>
                     </div>
