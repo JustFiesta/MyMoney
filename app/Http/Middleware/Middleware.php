a65b6ec145ec5fc8464b08b28499722e4c9462f5
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-27 15:41:49 +0200

    Fixup: role comperation was string but in db its a number

--- app/Http/Middleware/Middleware.php
+++ app/Http/Middleware/Middleware.php
@@ -16,7 +16,7 @@ class Middleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role_id === '1') {
+        if (Auth::check() && Auth::user()->role_id === 1) {
             return $next($request);
         }
         
