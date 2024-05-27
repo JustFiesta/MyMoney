d5c5dccd0cacdb543962c674aea0ee4ae86e7881
JustFiesta <99048876+JustFiesta@users.noreply.github.com>
2024-05-27 14:12:56 +0200

    Added checking fo admin in dropdown menu and admin panel links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,10 +20,7 @@
                     </x-nav-link>
                 </div>
             </div>
-
-
            
-
            <div class="flex items-center">
              {{-- Dark mode switch --}}
              <x-custom.theme-switcher />
@@ -33,7 +30,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->login }}</div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -47,6 +44,18 @@
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Ustawienia') }}
                         </x-dropdown-link>
+
+                        @if(Auth::user()->role_id == 1)
+                            <x-dropdown-link :href="route('admin.users')">
+                                {{ __('Panel Użytkowników') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('admin.goals')">
+                                {{ __('Panel Celi Użytkowników') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('admin.finances')">
+                                {{ __('Panel Finansów Użytkowników') }}
+                            </x-dropdown-link>
+                        @endif
 
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
@@ -89,7 +98,7 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->login }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
