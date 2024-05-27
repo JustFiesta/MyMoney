b832e9e5157bcf9d7383e9c5e12226324443bb31
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-27 20:31:58 +0200

    Remove billing period sketch div

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-2 md:py-4 lg:py-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="flex items-center justify-between p-3 md:p-6 lg:p-6 text-black-900 dark:text-gray-300">
-                    <p>Okres rozliczeniowy: {{ now()->translatedFormat('F') }}</p>
-                    ikona kalendarza
-                </div>
-            </div>
-        </div>
+    <div class="pt-2 md:pt-4 lg:pt-4">
     </div>
     
     @include('budget.index')
